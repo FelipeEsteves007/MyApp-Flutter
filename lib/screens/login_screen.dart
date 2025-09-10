8407a3f20af0ff8cdc3d8afac36bf6869c11157b
unknown <felipeestevesdepaula@gmail.com>
2025-09-10 17:04:42 -0300

    add screens e styles

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/styles/colors.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,12 +7,91 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      backgroundColor: AppColors.blue,
+      body: Stack(
         children: [
-          const Text("App de Investimentos"),
-          TextFormField(),
-          TextFormField(),
-          ElevatedButton(onPressed: (){}, child: Text("Entrar"))
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/logo.png", width: 200),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Text(
+                      "MetaInvest",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.gold,
+                        fontFamily: 'monospace',
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      decoration:  InputDecoration(
+                        label: Text(
+                          "E-mail",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                         enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: AppColors.lightGray)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(color:AppColors.gold)
+                        )
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    TextFormField(
+                      style: const TextStyle(color: Colors.white),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        label: Text(
+                          "Senha",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(color: AppColors.lightGray)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25),
+                          borderSide: BorderSide(color: AppColors.gold)
+                        )
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 55,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(context, "home");
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(AppColors.gold),
+                        ),
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/styles/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color blue = Color(0xFF021625);
+  static const Color lightBlue = Color(0xFF60A3D9);
+  static const Color gray = Color(0xFF2F3542);
+  static const Color lightGray = Color(0xFFF1F2F6);
+  static const Color gold = Color(0xFFF4B400);
+}
